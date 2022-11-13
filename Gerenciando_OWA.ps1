137aa9e783cccf3ddcb7060a57255f40d22e8313
Matheus Sales Da Silva <78692912+matheussdsi@users.noreply.github.com>
2022-11-13 11:44:53 -0300

    Update Gerenciando_OWA.ps1
    Este e um breve resumo sobre a  minha experiência.
    Disponibilizo a documentação oficial abaixo;
    https://learn.microsoft.com/pt-br/exchange/clients/outlook-on-the-web/mailbox-access?source=recommendations&view=exchserver-2019

--- Gerenciando_OWA.ps1
+++ Gerenciando_OWA.ps1
@@ -1,38 +1,18 @@
-Para Habilitar 
+Para Desabilitar o acesso via Web o comando é :
+
+Set-CasMailbox -Identity "AdeleV" -OWAEnabled $true
+
+Para Habilitar o acesso via Web o comando é: 
+
+Set-CasMailbox -Identity "MailboxIdentity" -OWAEnabled $false
 
 
+Para executar comando em massa podemos usar o seguinte comando(Realço que e necessario trocar a variavel #"$true ou False"#
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+Get-Content "C:\My Documents\Accounts.txt" | foreach {Set-CasMailbox $_ -OWAEnabled $false}
 
 Ativar OWA WebApp
 set-casmailbox "CAIXA AQUI " -owaEnabled $
 
-
-
+Comando para ver se ta tudo conforme o planejado após execução do comando.
+Get-CasMailbox -Identity "Usuário desejado"
